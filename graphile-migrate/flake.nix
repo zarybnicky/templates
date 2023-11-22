d1815c51284ed2227f0722ebdca6b35aeb434b8f
Jakub Zárybnický <jakub@zarybnicky.com>
2023-11-22 15:18:09 +0100

    Add graphile migrate template

---
+++ graphile-migrate/flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/release-23.05;
+  inputs.devenv.url = github:cachix/devenv;
+  inputs.graphile-migrate-flake.url = github:zarybnicky/graphile-migrate-flake;
+
+  outputs = { self, nixpkgs, devenv, graphile-migrate-flake, ... } @ inputs: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [
+        graphile-migrate-flake.overlays.default
+        self.overlays.default
+      ];
+    };
+  in {
+    devenv-up = self.devShells.x86_64-linux.default.config.procfileScript;
+    devShells.x86_64-linux.default = devenv.lib.mkShell {
+      inherit inputs pkgs;
+      modules = [
+        ({ pkgs, ... }: {
+          packages = [
+            pkgs.graphile-migrate
+            pkgs.postgresql_15
+            pkgs.sqlint
+            pkgs.pgformatter
+          ];
+
+          services = {
+            postgresql.enable = true;
+          };
+        })
+      ];
+    };
+  };
+}
